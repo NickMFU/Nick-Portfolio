6c9d99684cd49c7370e5dcec510a225ed269df95
NickMFU <6431503062@lamduan.mfu.ac.th>
2024-09-15 23:23:29 +0700

    first commit

--- javascript/valtilt.js
+++ javascript/valtilt.js
@@ -6,3 +6,16 @@ VanillaTilt.init(tilt, {
   glare: true,
   "max-glare": 0.3,
 });
+
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    hamburger.classList.toggle('open');
+    navItems.forEach((item, index) => {
+        item.style.setProperty('--i', index);
+    });
+});
